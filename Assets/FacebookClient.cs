737e2a860d0a082ee5bfa37306f2eccd88d95344
Panemiele <gabriele.romualdi98@gmail.com>
2020-06-04 18:43:55 +0200

    Testina

---
+++ Assets/FacebookClient.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+
+public class FacebookClient : MonoBehaviour {
+	public GameObject NotLoggedInUI;
+	public GameObject LoggedInUI;
+	public bool isLoggedIn;
+    private string FbFirstName;
+
+    void Awake(){
+		FB.Init (InitCallBack);
+	}
+
+	void InitCallBack(){
+		ShowUI ();
+	}
+
+	public void Login(){
+		FB.LogInWithReadPermissions (new List<string>{"public_profile", "email"}, LoginCallBack);
+	}
+
+	void LoginCallBack(IResult result){
+		if (string.IsNullOrEmpty(result.Error))
+			ShowUI ();
+		else
+			Debug.Log ("Error during login : " + result.Error);
+	}
+
+	public void Logout(){
+		FB.LogOut();
+		ShowUI ();
+	}
+
+	void ShowUI(){
+		if (FB.IsLoggedIn) {
+			NotLoggedInUI.SetActive (false);
+			LoggedInUI.SetActive (true);
+			this.GetComponent<SyncClient>().FBHasLoggedIn(AccessToken.CurrentAccessToken.TokenString, AccessToken.CurrentAccessToken.UserId);
+			FB.API ("me/picture?width=100&height=100", HttpMethod.GET, PictureCallBack);
+			FB.API ("me?fields=first_name", HttpMethod.GET, NameCallBack);
+			isLoggedIn = true;
+		} else {
+			NotLoggedInUI.SetActive (true);
+			LoggedInUI.SetActive (false);
+			isLoggedIn = false;
+		}
+	}
+
+	void PictureCallBack(IGraphResult result){
+		LoggedInUI.transform.Find("Image").GetComponentInChildren<Image> ().sprite = Sprite.Create (result.Texture, new Rect (0, 0, 100, 100), new Vector2 (0.5f, 0.5f));
+	}
+
+	void NameCallBack(IGraphResult result){
+		IDictionary<string,object>  profil = result.ResultDictionary;
+		FbFirstName = profil["first_name"].ToString();
+		Debug.Log("Ecco il nome: " + FbFirstName);
+		LoggedInUI.transform.Find("Name").GetComponent<Text>().text = "Benvenuto " + profil["first_name"].ToString() + "!";
+	}
+
+    public string GetFbFirstName()
+    {
+		return FbFirstName;
+    }
+}
---
+++ Assets/SyncClient.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+using Amazon;
+using Amazon.Runtime;
+using Amazon.CognitoIdentity;
+using Amazon.CognitoIdentity.Model;
+using Amazon.CognitoSync;
+using Amazon.CognitoSync.SyncManager;
+
+public class SyncClient : MonoBehaviour {
+	public GameObject LoggedInUI;
+	bool sync = false;
+	string name;
+	int health;
+	int exp;
+	Dataset playerInfo;
+	CognitoSyncManager syncManager;
+	CognitoAWSCredentials credentials;
+
+	void Start () {
+		UnityInitializer.AttachToGameObject (this.gameObject);
+		//Remove if you want to build on an IOS device.
+		AWSConfigs.LoggingConfig.LogTo = LoggingOptions.UnityLogger;
+		AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+		credentials = new CognitoAWSCredentials ("eu-central-1:a3ee931a-4748-432f-be7e-3446d3f370b0",
+                                                 RegionEndpoint.EUCentral1);
+		syncManager = new CognitoSyncManager (credentials, RegionEndpoint.EUCentral1);
+		playerInfo = syncManager.OpenOrCreateDataset ("playerInfo");
+		playerInfo.OnSyncSuccess += SyncSuccessCallback;
+	}
+
+	public void ChangeName(string newName){
+		name = newName;
+		playerInfo.Put ("name", newName);
+	}
+
+	public void ChangeHealth(string newHealth){
+		try{
+			health = int.Parse(newHealth);
+			playerInfo.Put("health", newHealth);
+		} catch{
+		}
+	}
+	
+	public void ChangeExp(string newExp){
+		try{
+			exp = int.Parse(newExp);
+			playerInfo.Put("exp", newExp);
+		} catch{
+		}
+	}
+
+	public void Synchronize(){
+		if (!string.IsNullOrEmpty (playerInfo.Get ("FacebookId")) && !this.GetComponent<FacebookClient> ().isLoggedIn) {
+			Debug.Log ("You must logged in to synchronize.");
+		} else {
+			sync = true;
+			playerInfo.SynchronizeOnConnectivity ();
+		}
+		UpdateInformation();
+	}
+
+	void UpdateInformation(){
+		if (!string.IsNullOrEmpty (playerInfo.Get ("name"))) {
+			name = playerInfo.Get("name");
+			LoggedInUI.transform.Find ("NameInputField").GetComponent<InputField> ().text = name;
+		} else
+			LoggedInUI.transform.Find ("NameInputField").GetComponent<InputField> ().text = "";
+		if (!string.IsNullOrEmpty (playerInfo.Get ("health"))) {
+			health = int.Parse(playerInfo.Get ("health"));
+			LoggedInUI.transform.Find ("HealthInputField").GetComponent<InputField> ().text = health.ToString();
+		} else
+			LoggedInUI.transform.Find ("HealthInputField").GetComponent<InputField> ().text = "";
+		if (!string.IsNullOrEmpty (playerInfo.Get ("exp"))) {
+			exp = int.Parse (playerInfo.Get ("exp"));
+			LoggedInUI.transform.Find ("ExpInputField").GetComponent<InputField> ().text = exp.ToString ();
+		} else
+			LoggedInUI.transform.Find ("ExpInputField").GetComponent<InputField> ().text = "";
+	}
+
+	void SyncSuccessCallback(object sender, SyncSuccessEventArgs e){
+		List<Record> newRecords = e.UpdatedRecords;
+		for (int k = 0; k < newRecords.Count; k++) {
+			Debug.Log (newRecords [k].Key + " was updated: " + newRecords [k].Value);
+		}
+		UpdateInformation ();
+		sync = false;
+	}
+
+
+	public void FBHasLoggedIn(string token, string id) {
+		string oldFacebookId = playerInfo.Get ("FacebookId");
+		if (string.IsNullOrEmpty(oldFacebookId) || id.Equals (oldFacebookId)) {
+			playerInfo.Put ("FacebookId", id);
+			playerInfo.Put("name", GetComponent<FacebookClient>().GetFbFirstName());
+			credentials.AddLogin ("graph.facebook.com", token);
+		} else {
+			Debug.Log ("New user detected.");
+			credentials.Clear ();
+			playerInfo.Delete ();
+			credentials.AddLogin ("graph.facebook.com", token);
+			Synchronize ();
+			StartCoroutine (WaitForEndOfSync (id));
+		}
+	}
+
+	IEnumerator WaitForEndOfSync(string id){
+		while (sync)
+			yield return null;
+		playerInfo.Put ("FacebookId", id);
+	}
+
+}
